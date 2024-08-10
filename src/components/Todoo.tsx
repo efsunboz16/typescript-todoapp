7a8172a84f37da20de81e4fcb61675e65d39c33a
efsunboz16 <devrim.boz16@gmail.com>
2024-08-10 12:22:36 +0300

    update todo

--- src/components/Todoo.tsx
+++ src/components/Todoo.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TiDelete } from "react-icons/ti";
 import { BsChatSquareText } from "react-icons/bs";
+import { FaCheck } from "react-icons/fa";
 import { TodoType } from '../types/Types';
 import { useDispatch } from 'react-redux';
-import { removeTodo } from '../redux/todoslice'
+import { removeTodo, updateTodo } from '../redux/todoslice'
 
 interface TodoProps {
     todoProps: TodoType
@@ -15,17 +16,29 @@ function Todo({ todoProps }: TodoProps) {
 
     const dispatch = useDispatch()
 
+    const [editable, setEditable] = useState<boolean>(false)
+    const [newTodo, setNewTodo] = useState<string>(content)
+
     const handleRemove = () => {
         dispatch(removeTodo(id))
+    }
+
+    const handleChange = () => {
+        const payload: TodoType = {
+            id: id,
+            content: newTodo
+        }
+        dispatch(updateTodo(payload))
+        setEditable(false)
     }
 
     return (
         <div className='w-full  '>
             <div className='flex justify-between w-full bg-slate-200 mt-4 h-16 items-center p-3 rounded-xl shadow-2xl'>
-                <p className='w-2/3 '>{content}</p>
+                {editable ? <input type='text' value={newTodo} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setNewTodo(e.target.value) }} /> : <p className='w-2/3 '>{content}</p>}
                 <div className='w-1/5 flex justify-between text-3xl'>
                     <TiDelete onClick={handleRemove} className='cursor-pointer' />
-                    <BsChatSquareText className='cursor-pointer' />
+                    {editable ? <FaCheck className='cursor-pointer' onClick={handleChange} /> : <BsChatSquareText className='cursor-pointer' onClick={() => setEditable(true)} />}
                 </div>
             </div>
         </div>
